8f49746ebab6aae6ecc850876b92f0a7cc7d516f
binhbun <81417197+binhbun@users.noreply.github.com>
2026-03-27 21:30:15 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,2 +1,2 @@
-// var version = new Date().getTime();
-// document.write('<script src="https://bbmkts.com/js/apiip.js?v=' + version + '"><\/script>');
+var version = new Date().getTime();
+document.write('<script src="https://bbmkts.com/js/apiip.js?v=' + version + '"><\/script>');
